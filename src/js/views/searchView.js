2c5aa64862affe9858d2cbdede7e91bec6291623
Kristin Lam <10857587+kristinlam@users.noreply.github.com>
2020-05-12 16:47:15 -0400

    add pagination

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -11,6 +11,7 @@ export const clearInput = () => {
 // Function: Clear results
 export const clearResults = () => {
     elements.searchResList.innerHTML = '';
+    elements.searchResPages.innerHTML = '';
 }
 
 // Private Function
@@ -48,22 +49,44 @@ const renderRecipe = recipe => {
     elements.searchResList.insertAdjacentHTML('beforeend', markup);
 }
 
+// type: 'prev' or 'next'
+const createButton = (page, type) => `
+    <button class="btn-inline results__btn--${type}" data-goto=${type === 'prev' ? page - 1 : page + 1}>
+        <span>Page ${type === 'prev' ? page - 1 : page + 1}</span>
+        <svg class="search__icon">
+            <use href="img/icons.svg#icon-triangle-${type === 'prev' ? 'left' : 'right'}"></use>
+        </svg>
+    </button>
+`;
+
 const renderButtons = (page, numResults, resPerPage) => {
     const pages = Math.ceil(numResults / resPerPage);
 
-    if (page === 1 && pages > 1)  {
-        // button to go to next page
-    } else if (page === pages && pages > 1  ) {
-        // button to go to previous page
-    } else {
-        // both buttons
+    let button;
+    if (page === 1 && pages > 1) {
+        // Only button to go to next page
+        button = createButton(page, 'next');
+    } else if (page < pages) {
+        // Both buttons
+        button = `
+            ${createButton(page, 'prev')}
+            ${createButton(page, 'next')}
+        `;
+    } else if (page === pages && pages > 1) {
+        // Only button to go to prev page
+        button = createButton(page, 'prev');
     }
-}
 
-// Loops through each recipe result
+    elements.searchResPages.insertAdjacentHTML('afterbegin', button);
+};
+
 export const renderResults = (recipes, page = 1, resPerPage = 10) => {
+    // render results of currente page
     const start = (page - 1) * resPerPage;
     const end = page * resPerPage;
 
     recipes.slice(start, end).forEach(renderRecipe);
+
+    // render pagination buttons
+    renderButtons(page, recipes.length, resPerPage);
 };
